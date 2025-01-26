a59e10fe3a6ca77da0f1dd3a2b5ddd3660107959
4tuna <4tuna.uno@gmail.com>
2025-01-26 21:58:16 +0900

    リファクタリング

--- content.js
+++ content.js
@@ -1,163 +1,180 @@
 'use strict';
 
-const zoom = 0.75; // ズーム倍率
-const currentUrl = window.location.href; // 現在のURL
+class ContentHandler {
+    zoom = 0.75; // ズーム倍率
 
-// メッセージリスナーの設定
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'scrapeSongs') {
-        const songs = fetchSongs();
-        sendResponse({ songs });
+    constructor() {
+        this.addEventListeners();
     }
-});
 
-// 楽曲データを取得する関数
-function fetchSongs() {
-    const songs = [];
-    const songElems = document.querySelectorAll('.tr-score');
-
-    for (const songElem of songElems) {
-        const difficultyElem = songElem.querySelector('.td-level .sp-difficulty');
-
-        const title = songElem.querySelector('.td-title').innerText.replace(/[\u200B\u200C\u200D]/g, '');
-        const id = getStringUntilHyphen(songElem.id);
-        const playStyle = getSongPlayStyle(difficultyElem);
-        const difficulty = parseInt(songElem.querySelector('.td-level').innerText, 10);
-        const chartType = getSongChartType(difficultyElem);
-
-        songs.push({
-            title: title,
-            id: id,
-            play_style: Number(playStyle),
-            difficulty: Number(difficulty),
-            chart_type: Number(chartType),
+    addEventListeners() {
+        // メッセージリスナーの設定
+        chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+            if (request.action === 'scrapeSongs') {
+                const songs = this.fetchSongs();
+                sendResponse({ songs });
+            }
         });
     }
 
-    // タイトル順でグループ化し、レベルを昇順にソートする
-    return sortSongsByTitleAndLevel(songs);
-}
+    async setupDifficultyListPage() {
+        // ページのズームを更新
+        document.body.style.zoom = this.zoom;
+        const difficultyListContainer = document.querySelector('#difficulty-list-container');
+        difficultyListContainer.style.height = '100%';
 
-function getStringUntilHyphen(str) {
-    const hyphenIndex = str.indexOf('-');
-    if (hyphenIndex === -1) {
-        return str;
+        // ブラウザリサイズ時、ズームを更新
+        window.addEventListener('resize', () => this.adjustLayout());
+        this.adjustLayout();
+
+        // 表示するカテゴリを変更するUIを追加
+        this.createCategoryCheckboxes();
+
+        // ジャケット要素にカテゴリアイコンを追加
+        (async () => {
+            const difficulty = parseInt(document.getElementById('select-level').value, 10);
+            const playStyle = parseInt(document.getElementById('select-spdp').value, 10);
+
+            const songs = await this.fetchSongsFromStorage();
+            const filteredSongs = songs.filter(song => song.difficulty === difficulty && song.play_style === playStyle + 1);
+
+            this.updateJacketCategories(filteredSongs);
+        })();
     }
-    return str.substring(0, hyphenIndex);
-}
 
-// プレイスタイルを取得する関数
-function getSongPlayStyle(difficultyElem) {
-    for (const className of difficultyElem.classList) {
-        if (className.startsWith('diff-color-')) {
-            const difficulty = parseInt(className.split('-')[2], 10);
-            return (difficulty >= 5) ? 2 : 1;
-        }
-    }
-}
-
-// 難易度タイプを取得する関数
-function getSongChartType(difficultyElem) {
-    for (const className of difficultyElem.classList) {
-        if (className.startsWith('diff-color-')) {
-            const difficulty = parseInt(className.split('-')[2], 10);
-            return (difficulty >= 5) ? difficulty - 4 : difficulty;
-        }
-    }
-}
-
-// 楽曲をタイトル順にグループ化し、レベルを昇順にソートする関数
-function sortSongsByTitleAndLevel(songs) {
-    const sortedSongs = songs.reduce((acc, current) => {
-        const group = acc.find(item => item.title === current.title);
-
-        if (group) {
-            group.items.push(current); // 同じタイトルのグループに追加
-        } else {
-            acc.push({ title: current.title, items: [current] }); // 新しいグループを作成
-        }
-
-        return acc;
-    }, []);
-
-    // 各グループのレベルを昇順にソート
-    return sortedSongs.flatMap(item => item.items.sort((a, b) => a.level - b.level));
-}
-
-// ページのレイアウト調整
-if (currentUrl.includes('/difficulty_list/')) {
-    // ページのズームを更新
-    document.body.style.zoom = zoom;
-    const difficultyListContainer = document.querySelector('#difficulty-list-container');
-    difficultyListContainer.style.height = '100%';
-
-    // ブラウザリサイズ時、ズームを更新
-    function adjustLayout() {
+    // ズームを更新
+    adjustLayout() {
         const mainFlexbox = document.querySelector('#main-flexbox');
-        mainFlexbox.style.height = (window.innerHeight / zoom) + 'px';
+        mainFlexbox.style.height = (window.innerHeight / this.zoom) + 'px';
     }
-    window.addEventListener('resize', adjustLayout);
-    adjustLayout();
 
-    // 表示するカテゴリを変更するUIを追加
-    createCategoryCheckboxes();
-
-    // ジャケット要素にカテゴリアイコンを追加
-    (async () => {
-        const difficulty = parseInt(document.getElementById('select-level').value, 10);
-        const playStyle = parseInt(document.getElementById('select-spdp').value, 10);
-
-        const songs = await fetchSongsFromStorage();
-        const filteredSongs = songs.filter(song => song.difficulty === difficulty && song.play_style === playStyle + 1);
-
-        updateJacketCategories(filteredSongs);
-    })();
-}
-
-// カテゴリのチェックボックスを作成する関数
-function createCategoryCheckboxes() {
-    const checkboxGroupHtml = `
+    // カテゴリのチェックボックスを作成する関数
+    createCategoryCheckboxes() {
+        const checkboxGroupHtml = `
         <div class="checkbox-group">
             <label><input type="checkbox" name="category" value="1" checked>CLASSIC</label>
             <label><input type="checkbox" name="category" value="2" checked>WHITE</label>
             <label><input type="checkbox" name="category" value="3" checked>GOLD</label>
         </div>
     `;
-    const checkboxGroup = (new DOMParser().parseFromString(checkboxGroupHtml, 'text/html')).body.firstElementChild;
+        const checkboxGroup = (new DOMParser().parseFromString(checkboxGroupHtml, 'text/html')).body.firstElementChild;
 
-    const categoryCheckboxes = checkboxGroup.querySelectorAll('[name="category"]');
-    for (const categoryCheckbox of categoryCheckboxes) {
-        categoryCheckbox.addEventListener('change', (e) => {
-            const category = categoryCheckbox.value;
-            const jackets = document.querySelectorAll(`.category-${category}`);
-            for (const jacket of jackets) {
-                jacket.style.display = e.target.checked ? '' : 'none';
-            }
-        });
+        const categoryCheckboxes = checkboxGroup.querySelectorAll('[name="category"]');
+        for (const categoryCheckbox of categoryCheckboxes) {
+            categoryCheckbox.addEventListener('change', (e) => {
+                const category = categoryCheckbox.value;
+                const jackets = document.querySelectorAll(`.category-${category}`);
+                for (const jacket of jackets) {
+                    jacket.style.display = e.target.checked ? '' : 'none';
+                }
+            });
+        }
+
+        const listHeader = document.querySelector('#difficulty-list-header');
+        listHeader.appendChild(checkboxGroup);
     }
 
-    const listHeader = document.querySelector('#difficulty-list-header');
-    listHeader.appendChild(checkboxGroup);
-}
+    // ストレージから楽曲データを取得する関数
+    async fetchSongsFromStorage() {
+        const data = await chrome.storage.local.get('songs');
+        return data.songs || []; // データが存在しない場合は空の配列を返す
+    }
 
-// ストレージから楽曲データを取得する関数
-async function fetchSongsFromStorage() {
-    const data = await chrome.storage.local.get('songs');
-    return data.songs || []; // データが存在しない場合は空の配列を返す
-}
+    // 楽曲データを取得する関数
+    fetchSongs() {
+        const songElems = document.querySelectorAll('.tr-score');
+        const songs = [];
 
-// ジャケットにカテゴリを設定する関数
-function updateJacketCategories(songs) {
-    const jackets = document.querySelectorAll('.div-jacket');
-    for (const jacket of jackets) {
-        const img = jacket.querySelector('img');
-        const title = img.getAttribute('title').trim();
+        for (const songElem of songElems) {
+            const difficultyElem = songElem.querySelector('.td-level .sp-difficulty');
 
-        const findSong = songs.find(song => song.title === title);
-        if (findSong) {
-            jacket.classList.add(`category-${findSong.category}`);
-        } else {
-            console.log(`no song: ${title}`);
+            const title = songElem.querySelector('.td-title').innerText.replace(/[\u200B\u200C\u200D]/g, '');
+            const id = this.getStringUntilHyphen(songElem.id);
+            const playStyle = this.getSongPlayStyle(difficultyElem);
+            const difficulty = parseInt(songElem.querySelector('.td-level').innerText, 10);
+            const chartType = this.getSongChartType(difficultyElem);
+
+            songs.push({
+                title: title,
+                id: id,
+                play_style: Number(playStyle),
+                difficulty: Number(difficulty),
+                chart_type: Number(chartType),
+            });
+        }
+
+        // タイトル順でグループ化し、レベルを昇順にソートする
+        return this.sortSongsByTitleAndLevel(songs);
+    }
+
+    // 文字列のハイフンまでを取得
+    getStringUntilHyphen(str) {
+        const hyphenIndex = str.indexOf('-');
+        if (hyphenIndex === -1) {
+            return str;
+        }
+        return str.substring(0, hyphenIndex);
+    }
+
+    // プレイスタイルを取得する関数
+    getSongPlayStyle(difficultyElem) {
+        for (const className of difficultyElem.classList) {
+            if (className.startsWith('diff-color-')) {
+                const difficulty = parseInt(className.split('-')[2], 10);
+                return (difficulty >= 5) ? 2 : 1;
+            }
         }
     }
-}
+
+    // 難易度タイプを取得する関数
+    getSongChartType(difficultyElem) {
+        for (const className of difficultyElem.classList) {
+            if (className.startsWith('diff-color-')) {
+                const chartType = parseInt(className.split('-')[2], 10);
+                return (chartType >= 5) ? chartType - 4 : chartType;
+            }
+        }
+    }
+
+    // 楽曲をタイトル順にグループ化し、レベルを昇順にソートする関数
+    sortSongsByTitleAndLevel(songs) {
+        const sortedSongs = songs.reduce((acc, current) => {
+            const group = acc.find(item => item.title === current.title);
+
+            if (group) {
+                group.items.push(current); // 同じタイトルのグループに追加
+            } else {
+                acc.push({ title: current.title, items: [current] }); // 新しいグループを作成
+            }
+
+            return acc;
+        }, []);
+
+        // 各グループのレベルを昇順にソート
+        return sortedSongs.flatMap(item => item.items.sort((a, b) => a.level - b.level));
+    }
+
+    // ジャケットにカテゴリを設定する関数
+    updateJacketCategories(songs) {
+        const jackets = document.querySelectorAll('.div-jacket');
+        for (const jacket of jackets) {
+            const img = jacket.querySelector('img');
+            const title = img.getAttribute('title').trim();
+
+            const findSong = songs.find(song => song.title === title);
+            if (findSong) {
+                jacket.classList.add(`category-${findSong.category}`);
+            } else {
+                console.log(`no song: ${title}`);
+            }
+        }
+    }
+};
+
+(async () => {
+    const contentHandler = new ContentHandler();
+
+    if (window.location.href.includes('/difficulty_list/')) {
+        await contentHandler.setupDifficultyListPage();
+    }
+})();
--- popup.js
+++ popup.js
@@ -1,110 +1,129 @@
 'use strict';
 
-const categoryNames = [null, 'CLASSIC', 'WHITE', 'GOLD'];
-const updateButtons = document.querySelectorAll('.update-button');
-const copySongsJsonButton = document.querySelector('.clipboard-to-json');
-const clearSongsButton = document.querySelector('.clear-songs');
+class PopupManager {
+    static PLAY_STYLE_NAMES = [null, 'SP', 'DP'];
+    static CATEGORY_NAMES = [null, 'CLASSIC', 'WHITE', 'GOLD'];
 
-// 楽曲データを更新する関数
-async function updateSongs(songs, playStyle, category) {
-    try {
-        // songsにパラメータを追加
-        const newSongs = songs.map(song => ({
-            ...song,
-            category: category,
-        }));
+    updateButtons = null;
+    copySongsJsonButton = null;
+    clearSongsButton = null;
 
-        // 現在の楽曲データを取得
-        const storedSongs = await chrome.storage.local.get('songs');
-        let currentSongs = storedSongs.songs || []; // 未定義の場合は空の配列を使用
-
-        // 指定したカテゴリに該当する楽曲を除外
-        const filteredStoredSongs = currentSongs.filter(song => {
-            return !(song.playStyle === playStyle && song.category === category);
-        });
-
-        // 新しい楽曲を追加
-        const updatedSongs = [...filteredStoredSongs, ...newSongs];
-
-        // 更新された楽曲データを保存
-        await chrome.storage.local.set({ songs: updatedSongs });
-
-        // 更新完了メッセージを表示
-        alert(`${categoryNames[category]}の楽曲を更新しました。`);
-    } catch (error) {
-        alert('楽曲の更新に失敗しました。');
+    constructor() {
+        this.initialize();
+        this.addEventListeners();
     }
-}
 
-// 楽曲データの取得を行う関数
-async function fetchSongsFromActiveTab() {
-    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-    const activeTab = tabs[0];
+    initialize() {
+        this.updateButtons = document.querySelectorAll('.update-button');
+        this.copySongsJsonButton = document.querySelector('.clipboard-to-json');
+        this.clearSongsButton = document.querySelector('.clear-songs');
+    }
 
-    return new Promise((resolve, reject) => {
-        chrome.tabs.sendMessage(activeTab.id, {
-            action: 'scrapeSongs',
-        }, (response) => {
-            if (chrome.runtime.lastError) {
-                reject(chrome.runtime.lastError);
-            } else {
-                resolve(response);
-            }
-        });
-    });
-}
+    addEventListeners() {
+        // 更新ボタンに対するクリックイベントリスナーを設定
+        for (const updateButton of this.updateButtons) {
+            // 更新ボタンのクリック時処理
+            updateButton.addEventListener('click', async () => {
+                const playStyle = parseInt(updateButton.dataset.playStyle, 10);
+                const category = parseInt(updateButton.dataset.category, 10);
+                const confirmMessage = `${PopupManager.PLAY_STYLE_NAMES[playStyle]} ${PopupManager.CATEGORY_NAMES[category]}を更新します。よろしいですか？`;
 
-// 更新ボタンに対するクリックイベントリスナーを設定
-for (const updateButton of updateButtons) {
-    const playStyle = parseInt(updateButton.dataset['play-style'], 10);
-    const category = parseInt(updateButton.dataset.category, 10);
-    const confirmMessage = `${categoryNames[category]}を更新します。よろしいですか？`;
+                if (confirm(confirmMessage)) {
+                    try {
+                        const response = await this.fetchSongsFromActiveTab();
 
-    // 更新ボタンのクリック時処理
-    updateButton.addEventListener('click', async () => {
-        if (confirm(confirmMessage)) {
+                        // レスポンスが存在する場合、楽曲データを更新
+                        if (response && response.songs) {
+                            await this.updateSongs(response.songs, playStyle, category);
+                        } else {
+                            alert('更新に失敗しました。');
+                        }
+                    } catch (error) {
+                        alert('楽曲の更新中にエラーが発生しました。');
+                    }
+                }
+            });
+        }
+
+        // 楽曲データをJSON形式でクリップボードにコピーするボタンのクリックイベントリスナー
+        this.copySongsJsonButton.addEventListener('click', async () => {
             try {
-                const response = await fetchSongsFromActiveTab();
+                const data = await chrome.storage.local.get('songs');
 
-                // レスポンスが存在する場合、楽曲データを更新
-                if (response && response.songs) {
-                    await updateSongs(response.songs, playStyle, category);
+                // データが存在するか確認
+                if (data.songs && data.songs.length > 0) {
+                    const json = JSON.stringify(data.songs, null, 4);
+                    await navigator.clipboard.writeText(json);
+                    alert('楽曲データをクリップボードにコピーしました。');
                 } else {
-                    alert('更新に失敗しました。');
+                    alert('コピーする楽曲データが存在しません。');
                 }
             } catch (error) {
-                alert('楽曲の更新中にエラーが発生しました。');
+                alert('クリップボードへのコピーに失敗しました。');
             }
+        });
+
+        // 楽曲データをクリアするボタンのクリックイベントリスナー
+        this.clearSongsButton.addEventListener('click', async () => {
+            chrome.storage.local.remove('songs', () => {
+                if (chrome.runtime.lastError) {
+                    console.error("エラー:", chrome.runtime.lastError);
+                    alert('楽曲データの削除に失敗しました。');
+                } else {
+                    alert('楽曲データが削除されました。');
+                }
+            });
+        });
+    }
+
+    // 楽曲データの取得を行う関数
+    async fetchSongsFromActiveTab() {
+        const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+        const activeTab = tabs[0];
+
+        return new Promise((resolve, reject) => {
+            chrome.tabs.sendMessage(activeTab.id, {
+                action: 'scrapeSongs',
+            }, (response) => {
+                if (chrome.runtime.lastError) {
+                    reject(chrome.runtime.lastError);
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+    }
+
+    // 楽曲データを更新する関数
+    async updateSongs(songs, playStyle, category) {
+        try {
+            // songsにパラメータを追加
+            const newSongs = songs.map(song => ({
+                ...song,
+                category: category,
+            }));
+
+            // 現在の楽曲データを取得
+            const storedSongs = await chrome.storage.local.get('songs');
+            let currentSongs = storedSongs.songs || []; // 未定義の場合は空の配列を使用
+
+            // 指定したカテゴリに該当する楽曲を除外
+            const filteredStoredSongs = currentSongs.filter(song => {
+                return !(song.playStyle === playStyle && song.category === category);
+            });
+
+            // 新しい楽曲を追加
+            const updatedSongs = [...filteredStoredSongs, ...newSongs];
+
+            // 更新された楽曲データを保存
+            await chrome.storage.local.set({ songs: updatedSongs });
+
+            // 更新完了メッセージを表示
+            alert(`${PopupManager.PLAY_STYLE_NAMES[playStyle]} ${PopupManager.CATEGORY_NAMES[category]}の楽曲を更新しました。`);
+        } catch (error) {
+            alert('楽曲の更新に失敗しました。');
         }
-    });
+    }
 }
 
-// 楽曲データをJSON形式でクリップボードにコピーするボタンのクリックイベントリスナー
-copySongsJsonButton.addEventListener('click', async () => {
-    try {
-        const data = await chrome.storage.local.get('songs');
-
-        // データが存在するか確認
-        if (data.songs && data.songs.length > 0) {
-            const json = JSON.stringify(data.songs, null, 4);
-            await navigator.clipboard.writeText(json);
-            alert('楽曲データをクリップボードにコピーしました。');
-        } else {
-            alert('コピーする楽曲データが存在しません。');
-        }
-    } catch (error) {
-        alert('クリップボードへのコピーに失敗しました。');
-    }
-});
-
-// 楽曲データをクリアするボタンのクリックイベントリスナー
-clearSongsButton.addEventListener('click', async () => {
-    chrome.storage.local.remove('songs', () => {
-        if (chrome.runtime.lastError) {
-            console.error("エラー:", chrome.runtime.lastError);
-            alert('楽曲データの削除に失敗しました。');
-        } else {
-            alert('楽曲データが削除されました。');
-        }
-    });
-});
+new PopupManager();
